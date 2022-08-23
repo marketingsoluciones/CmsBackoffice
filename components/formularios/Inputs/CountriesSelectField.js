d13f6b0dcf22ac16fb385c7479a23995f8e3e3ee
Jafet <febmerlib@gmail.com>
2022-08-23 16:12:27 +0000

    BODAPP-127 fix translation of the name of the countries

--- components/formularios/Inputs/CountriesSelectField.js
+++ components/formularios/Inputs/CountriesSelectField.js
@@ -4,15 +4,16 @@ import { memo } from "react";
 import { useCallback, useEffect, useState } from "react";
 import { api } from 'utils/api'
 
-export const CounstriesSelectField = memo(({label, ...props}) => {
+export const CounstriesSelectField = memo(({ label, ...props }) => {
 
     const [field, meta, helpers] = useField(props);
     const [data, setData] = useState([])
 
-    const fetching = useCallback(async() => {
+    const fetching = useCallback(async () => {
         try {
-            const {data} = await api.getAllCountries()
-            const map = data?.map(item => ({name : item.name.common}))
+            const { data } = await api.getAllCountries()
+            const map = data?.map(item => ({ name: item?.translations?.spa?.common.toLowerCase() }))
+            console.log(map)
             setData(map)
         } catch (error) {
             console.log(error);
@@ -22,15 +23,15 @@ export const CounstriesSelectField = memo(({label, ...props}) => {
     useEffect(() => {
         fetching()
     }, []);
-    
 
-    return(
+
+    return (
         <Box>
             <Divider />
             <FormLabel paddingTop={"1rem"} fontWeight={"900"} textAlign={"left"} fontSize={"sm"}>{label}</FormLabel>
             <Select variant={"filled"} fontSize={"sm"} {...field} {...props} >
                 <option disabled={field.value === "" ? false : true}>Seleccionar</option>
-                {data?.sort((a,b) => a.name.localeCompare(b.name))?.map((item,idx) => (
+                {data?.sort((a, b) => a.name.localeCompare(b.name))?.map((item, idx) => (
                     <option key={idx} value={item.name}>{item.name}</option>
                 ))}
             </Select>
