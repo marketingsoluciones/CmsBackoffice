25f9c32e351138d007e9d3cb4d0717e3aa2c3c95
eduardo andres <adch.edch@gmail.com>
2023-03-15 04:51:44 +0500

    fix comportamiento de items en el sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -22,7 +22,7 @@ import { ArrowDownIcon } from "./../components/Icons/index";
 export const Sidebar = ({ state }) => {
   const { user, development, setDevelopment } = AuthContextProvider()
   const { asPath } = useRouter()
-
+  
   const Options = [
     {
       title: "Bodasdehoy.com",
@@ -55,41 +55,40 @@ export const Sidebar = ({ state }) => {
     >
 
       <Flex alignItems={"center"} gap={"0.5rem"} p={"0.5rem"}>
-
         <Tooltip label={`${state ? "" : development}`} ml="14" top="-10">
-
           <div className={`flex  ${state ? "justify-star" : " justify-end "} items-center gap-2 w-full bg-gray-100 py-2 px-2 rounded-xl`}>
-
             <div className={`${state ? "hidden" : "block"}`} >
-              <Menu  >
+              <Menu autoSelect={false}  >
                 <MenuButton pr={"0.3rem"}>
                   <Avatar size={"sm"} />
                 </MenuButton>
                 <MenuList p={"0"} fontSize={"sm"} ml={"8"}>
                   {Options.map((item, idx) => (
                     <div key={idx}>
-                      <MenuItem color={"gray.500"} onClick={() => setDevelopment(item.value)}>{item.title}</MenuItem>
+                      <MenuItem style={item.value === development ? { backgroundColor: '#F3F3F3' } : { backgroundColor: '' }} color={"gray.500"} onClick={() => setDevelopment(item.value)}>{item.title}</MenuItem>
                     </div>
                   ))}
                 </MenuList>
               </Menu>
             </div>
 
-            <div className={`flex items-center justify-star ${state ? "block" : "hidden"}`}>
-              <Avatar size={"sm"} mr={"0.1rem"} />
-              <div className={``}>
+            <div className={`flex items-center justify-star w-full ${state ? "block" : "hidden"}`}>
+              <Avatar size={"sm"} mr={"0.4rem"} />
+              <div className={`w-full`}>
                 <Text pl={"0.1rem"} className={` text-tituloPrimario`}>{"Consorcio J.C"}</Text>
                 <div className="flex">
-                  <Menu >
-                    <MenuButton w={"8rem"}>
-                      <Text noOfLines={1} textAlign={"start"} ml={"2px"}>{development}</Text>
+                  <Menu autoSelect={false} foc >
+                    <MenuButton flex={"2"} w={"8rem"}>
+                      <div className="flex justify-between w-full">
+                        <Text noOfLines={1} textAlign={"start"} ml={"2px"} >{development}</Text>
+                        <ArrowDownIcon h={2} w={3} />
+                      </div>
                     </MenuButton>
-                    <ArrowDownIcon h={2} w={3} />
-                    <MenuList p={"0"} fontSize={"sm"} ml={""} justifyItems={"start"}>
+                    <MenuList p={"0"} fontSize={"sm"} justifyItems={"start"}>
                       {Options.map((item, idx) => (
-                        <div key={idx}>
-                          <MenuItem color={"gray.500"} onClick={() => setDevelopment(item.value)}>{item.title}</MenuItem>
-                        </div>
+
+                        <MenuItem key={idx} style={item.value === development ? { backgroundColor: '#F3F4F6' } : { backgroundColor: '' }} color={"gray.500"} onClick={() => setDevelopment(item.value)}>{item.title}</MenuItem>
+
                       ))}
                     </MenuList>
                   </Menu>
@@ -106,37 +105,48 @@ export const Sidebar = ({ state }) => {
             return (
               <>
                 {
-                  BodyStaticAPP.map((item, idx) => (
+                  BodyStaticAPP.map((item, idx) => {
+                  
+                    return(
+                   
                     <Box key={idx} >
-                      <Menu>
+                      <Menu autoSelect={false}>
                         <MenuGroup key={idx} title={item.title} fontSize={"sm"} className={` ${state ? "block" : "hidden"} text-tituloPrimario`}>
-                          {item.children.map((item, idx) => (
-                            <Link key={idx} href={item.route}>
-                              <MenuItem
-                                key={idx}
-                                color={"#637381"}
-                                padding={`${state ? "2" : ""}`}
-                                marginLeft={"2"}
-                                w={"95%"}
-                                fontSize={"sm"}
-                                className={` flex  ${state ? "justify-star" : "justify-end"} items-center w-full rounded-md `}
-                              >
-                                <Tooltip label={`${state ? "" : item.title}`} ml="14" top="-10">
-                                  <div className={`flex justify-estar ${state ? "" : `relative`}`} data-tip={`${item.title}`}>
-                                    <div className={` pr-2 `}>{item.icon}</div>
-                                    <div className={`${state ? "block " : "hidden"}`}>
-                                      {item.title}
-                                    </div>
-                                  </div>
-                                </Tooltip>
+                          {item.children.map((item, idx) => {
+                            return (
 
-                              </MenuItem>
-                            </Link>
-                          ))}
+                              <Link key={idx} href={item.route}>
+                                <MenuItem
+                                  _hover={{ bg: "#F3F3F3" }}
+                                  key={idx}
+                                  color={"#637381"}
+                                  padding={`${state ? "2" : ""}`}
+                                  marginLeft={"2"}
+                                  w={"95%"}
+                                  fontSize={"sm"}
+                                  className={` flex  ${state ? "justify-star" : "justify-end"} items-center w-full rounded-md `}
+                                  style={"/" + item.route === asPath ? { backgroundColor: '#F3F3F3' } : { backgroundColor: '' } && item.route === asPath ? { backgroundColor: '#F3F3F3' } : { backgroundColor: '' }}
+
+                                >
+                                  <Tooltip label={`${state ? "" : item.title}`} ml="14" top="-10">
+                                    <div className={`flex justify-estar items-center  ${state ? "" : `relative`}`} data-tip={`${item.title}`}>
+                                      <div className={` pr-2 `}>
+                                        {item.icon}
+                                      </div>
+                                      <div className={`${state ? "block " : "hidden"}`}>
+                                        {item.title}
+                                      </div>
+                                    </div>
+                                  </Tooltip>
+
+                                </MenuItem>
+                              </Link>
+                            )
+                          })}
                         </MenuGroup>
                       </Menu>
                     </Box>
-                  ))
+                  )})
                 }
               </>
             )
@@ -146,11 +156,12 @@ export const Sidebar = ({ state }) => {
                 {
                   CivitasStaticAPP.map((item, idx) => (
                     <Box key={idx}  >
-                      <Menu>
+                      <Menu autoSelect={false}>
                         <MenuGroup key={idx} title={item.title} fontSize={"sm"} className={` ${state ? "block" : "hidden"} text-tituloPrimario`}>
                           {item.children.map((item, idx) => (
                             <Link key={idx} href={item.route}>
                               <MenuItem
+                                _hover={{ bg: "#F3F3F3" }}
                                 key={idx}
                                 color={"#637381"}
                                 padding={`${state ? "2" : ""}`}
@@ -158,6 +169,7 @@ export const Sidebar = ({ state }) => {
                                 w={"95%"}
                                 fontSize={"sm"}
                                 className={` flex  ${state ? "justify-star" : "justify-end"} items-center w-full rounded-md `}
+                                style={"/" + item.route === asPath ? { backgroundColor: '#F3F3F3' } : { backgroundColor: '' }}
                               >
                                 <Tooltip label={`${state ? "" : item.title}`} ml="14" top="-10">
                                   <div className={`flex justify-estar ${state ? "" : `relative`}`} data-tip={`${item.title}`}>
