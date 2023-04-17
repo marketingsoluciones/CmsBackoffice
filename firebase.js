ede7d4336ff104ba130ecdffca6b082db626a862
Jafet <febmerlib@gmail.com>
2023-04-17 23:01:35 +0000

    probando

--- firebase.js
+++ firebase.js
@@ -1,6 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB-b-DmN66gmSnQFp5oA7RARmcgHgjX5Xo",
+  authDomain: "adminbodas.firebaseapp.com",
+  projectId: "adminbodas",
+  storageBucket: "adminbodas.appspot.com",
+  messagingSenderId: "75913806496",
+  appId: "1:75913806496:web:a2c8f56185e8b7239e3fc5",
+  measurementId: "G-C1MK53CP18"
+};
+
+const firebaseClient = initializeApp(firebaseConfig);
+
+console.log(2100, firebaseClient)
 
 
 const firebaseConfigBodas = {
