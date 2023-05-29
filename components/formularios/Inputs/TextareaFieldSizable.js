ca4cc77f7243731a387843f5c6d843ad5fac74e2
Jafet <febmerlib@gmail.com>
2023-05-29 01:24:27 +0000

    BODAPP-496 fixed

--- components/formularios/Inputs/TextareaFieldSizable.js
+++ components/formularios/Inputs/TextareaFieldSizable.js
@@ -1,12 +1,13 @@
 import { Box, Divider, Textarea, Flex, Text } from "@chakra-ui/react"
 import { useField } from "formik";
 import { FormLabelMod } from "./FormLabelMod";
-import { useRef, } from "react";
+import { useEffect, useRef, useState, } from "react";
 
 
 export const TextareaFieldSizable = ({ label, ...props }) => {
   const refInput = useRef(null)
   const [field, meta, helpers] = useField(props);
+  const [rows, setRows] = useState(1)
 
   const handleChange = (e) => {
     e.preventDefault()
@@ -20,6 +21,17 @@ export const TextareaFieldSizable = ({ label, ...props }) => {
     }
     helpers.setValue(e.target.value)
   }
+  useEffect(() => {
+    console.log("cambio")
+    const rowT = refInput?.current ? (refInput?.current.scrollHeight / 16) - 1 : 1
+    if (rowT < 5) {
+      setRows(rowT)
+    }
+    else {
+      setRows(4)
+    }
+  }, [refInput])
+
 
   return (
     <Box >
@@ -36,7 +48,7 @@ export const TextareaFieldSizable = ({ label, ...props }) => {
         <Box gap={"0.3rem"} alignItems={"center"}>
           <Textarea
             resize={"none"}
-            rows={1}
+            rows={rows}
             ref={refInput}
             className="outline-none overflow-y-scroll"
             onChange={(e) => { handleChange(e) }}
