d8067397da48640c644de8de1d5cc8ed0b7484c4
eduardo andres <adch.edch@gmail.com>
2023-10-17 16:34:48 -0400

    refactorin info page PE LB

--- components/LugaresBodasComponents/PlanoEvento.js
+++ components/LugaresBodasComponents/PlanoEvento.js
@@ -6,28 +6,41 @@ import { InfoPlanoEventoPage } from "./PlanoEventoComponents/InfoPlanoEventoPage
 export const PlanoEvento = () => {
     const [state, setState] = useState(true)
     const [state2, setState2] = useState(true)
+    const [optionSelect, setOptionSelect] = useState(0)
+    const dataComponents = [
+        {
+            component: <PlanoEventoTable />
+        },
+        {
+            component: ""
+        },
+
+    ]
 
     return (
         <div className="px-5 py-2 h-full">
             {(() => {
-                if (!state) {
+                if (state) {
                     return (
-                        <VistaSinDatos
-                            title={"Plano del Evento"}
-                            button={"Agregar Plano"}
-                            text={"Aún no tienes Planos guardadas"}
-                            accion={"añade tu Plano"}
-                        />
+                        <InfoPlanoEventoPage actionButton={state2} setActionButton={setState2} />
+
                     )
                 } else {
                     if (state2) {
                         return (
-                            <InfoPlanoEventoPage actionButton={state2} setActionButton={setState2}  />
+                            <div>
+                                {dataComponents[optionSelect].component}
+                            </div>
                         )
                     } else {
 
                         return (
-                            <PlanoEventoTable />
+                            <VistaSinDatos
+                                title={"Plano del Evento"}
+                                button={"Agregar Plano"}
+                                text={"Aún no tienes Planos guardadas"}
+                                accion={"añade tu Plano"}
+                            />
                         )
                     }
                 }
--- components/LugaresBodasComponents/PlanoEventoComponents/InfoPlanoEventoPage.js
+++ components/LugaresBodasComponents/PlanoEventoComponents/InfoPlanoEventoPage.js
@@ -1,44 +1,61 @@
+import { CorazoncirculoIcon, DiamanteIcon } from "../../Icons/index"
 
-export const InfoPlanoEventoPage = ({actionButton, setActionButton}) => {
+export const InfoPlanoEventoPage = ({ actionButton, setActionButton }) => {
     return (
         <div className="h-full ">
             <p className=" text-slate-600 mt-1 text-3xl text-rosa">
-                Plano del evento
+                Plano Del Evento
             </p>
             <div className="bg-white rounded-xl flex flex-col h-[91%]   " >
-                <div className="grid grid-cols-3 px-5  py-8 content-center ">
+                <div className="  flex items-center justify-center mt-3 ">
+                    <img src="/PlanoEvento.png" alt="alta" className="w-[95%] h-full" />
+                </div>
+
+                <div className="grid grid-cols-3 px-5  py-5 content-center ">
                     <div className=" col-span-1 space-y-2  flex flex-col justify-center items-center ">
-                        <p className="text-xl text-rosa font-semibold">
-                            Asigna y personaliza
-                        </p>
-                        <p>
-                            el plano de tu evento
+                        <p className="text-xl text-end ">
+                            <span className="text-rosa font-semibold"> Asigna un plano </span> a tu <br /> evento y organízalo
                         </p>
                     </div>
-                    <div className=" col-span-2 flex justify-center px-6">
+
+                    <div className=" col-span-2 flex justify-center items-center px-6">
                         <p className="text-base">
-                            Ordena la distribución de las mesas, el espacio para el funcionamiento de cada proveedor y sienta a los invitados. Todo lo que necesitas es diseñar el plano de tu evento en tu EventosOrganizador.
+                            Ordena la distribución de las mesas, el espacio para el funcionamiento de cada proveedor y sienta a los invitados de tu evento.
                         </p>
                     </div>
                 </div>
-                <div className="  flex items-center justify-center ">
-                    <img src="/plano.png" alt="alta" className="w-[95%] h-full" />
-                </div>
-                <div className="flex flex-col items-center justify-center px-10 py-7 ">
-                    <p className="text-base text-center ">Además de facilitar el cálculo optimo del salón para tu fiesta, te permite integrar a los proveedores que requieren participar en este plano. La ubicación del Dj, fotógrafo, el sector de recepción o de ceremonia. </p>
-                </div>
-                <div className="bg-gray-200 h-full rounded-b-lg  grid grid-cols-3 justify-items-center p-8 ">
-                    <div className="col-span-2 flex justify-center">
-                        <p className="w-[90%]">
-                            <span className="text-rosa">Enlaza tu plano de salón al evento </span>
-                            y lleva el control de cada una de las piezas de tu celebración ¡ahora mismo!
+
+                <div className="bg-gray-200 h-full grid grid-cols-3 justify-items-center content-center px-8 py-8 ">
+
+                    <div className="col-span-2 flex justify-center items-center text-end">
+                        <p className="w-[70%] text-base">
+                            <span className="text-rosa">
+                                Además de facilitar el cálculo óptimo del salón,
+                            </span>
+                            tendrás a la mano el listado de invitados por mesa y ubicación para mantener el orden de tu evento.
                         </p>
                     </div>
-                    <div className="col-span-1 flex items-center">
-                        <button onClick={()=> setActionButton(!actionButton)} className="bg-rosa text-base text-white px-2 py-1 rounded-lg">
-                            Gestionar planos
+
+                    <div className="col-span-1 flex flex-col justify-center items-center">
+                        <button onClick={() => setActionButton(!actionButton)} className="bg-rosa text-base text-white px-2 py-1 rounded-lg">
+                            Inicia prueba gratis de 30 días
                         </button>
+                        <div className=" flex items-center justify-center space-x-1 my-2 text-base cursor-default">
+                        <div className="text-amarillo">
+                                <CorazoncirculoIcon/>
+                            </div>
+                            <div className="text-amarillo">
+                                <DiamanteIcon />
+                            </div>
+                            <p className="text-rosa">
+                                Activa la versión  <span className="font-semibold"> BÁSICA O PREMIUM </span>
+                            </p>
+                        </div>
                     </div>
+                </div>
+
+                <div className="flex  items-center justify-center px-10 h-full ">
+                    <p className="text-base text-center ">Enlaza tu plano de salón al evento y lleva el control de cada una de las piezas de tu celebración.  </p>
                 </div>
             </div>
         </div>
