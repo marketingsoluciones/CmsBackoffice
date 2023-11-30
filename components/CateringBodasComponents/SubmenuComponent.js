3fdea3a6cd36f3ac1d91a0eeaf2da3475f3c0cc1
eduardo andres <adch.edch@gmail.com>
2023-11-30 16:13:42 -0400

    fix

--- components/CateringBodasComponents/SubmenuComponent.js
+++ components/CateringBodasComponents/SubmenuComponent.js
@@ -1,17 +1,30 @@
 import { EventoSelect } from "../EventoSelect"
 import { EventsGroupContextProvider } from "../../context/EventsGroupContext"
+import { ModalLeft } from "../modals/ModalLeft"
+import { FormCrearEvento } from "../EventosComponents/FormCrearEvento"
+import { useState } from "react"
 
-export const SubmenuComponent = ({dataComponents,optionSelect,onClick }) => {
-   const {eventsGroup} = EventsGroupContextProvider()
-    
+export const SubmenuComponent = ({ dataComponents, optionSelect, onClick }) => {
+    const { eventsGroup } = EventsGroupContextProvider()
+    const [isMounted, setIsMounted] = useState(false);
+
+
     return (
+
         <>
+            {(
+                <ModalLeft state={isMounted} set={setIsMounted}>
+                    {
+                        <FormCrearEvento state={isMounted} set={setIsMounted} />
+                    }
+                </ModalLeft>
+            )}
             <div className=" hidden md:block bg-gray-200 px-4 py-10 space-y-5 col-span-1">
-               { eventsGroup?.length != 0 ? <EventoSelect/>: null}
+                {eventsGroup?.length != 0 ? <EventoSelect /> : <ButtonEventForm isMounted={isMounted} setIsMounted={setIsMounted} />}
                 {
                     dataComponents.map((item, idx) => {
                         return (
-                            <div  key={idx} onClick={() => onClick(idx)} className={`${optionSelect === idx ? " text-rosa ": ""}flex  items-center  space-x-3 cursor-pointer  `}>
+                            <div key={idx} onClick={() => onClick(idx)} className={`${optionSelect === idx ? " text-rosa " : ""}flex  items-center  space-x-3 cursor-pointer  `}>
                                 <div className="h-full">
                                     {
                                         item.icon
@@ -29,5 +42,11 @@ export const SubmenuComponent = ({dataComponents,optionSelect,onClick }) => {
             </div>
 
         </>
+    )
+}
+
+const ButtonEventForm = ({setIsMounted,isMounted }) => {
+    return (
+        <button onClick={()=>setIsMounted(!isMounted)} className="text-base bg-rosa text-white rounded-lg p-2 w-full">Crea un Evento</button>
     )
 }
