e9415f090c1ba1a590f9ca22885b98dc6f467f55
eduardo andres <adch.edch@gmail.com>
2023-11-30 14:05:31 -0400

    working

--- components/Navigation.js
+++ components/Navigation.js
@@ -66,9 +66,9 @@ export const Navigation = ({ set, state, }) => {
           rout: window.origin.includes("://test") ? process.env.NEXT_PUBLIC_DIRECTORY?.replace("//", "//test.") : process.env.NEXT_PUBLIC_DIRECTORY
         },
         {
-          icon: <RedireccionIcon/>,
+          icon: <RedireccionIcon />,
           title: "Ir a AppBodasdehoy.com",
-          rout:  window.origin.includes("://test.") ? process.env.NEXT_PUBLIC_EVENTSAPP?.replace("//", "//test") ?? "" : process.env.NEXT_PUBLIC_EVENTSAPP ?? " "
+          rout: cookieContent?.eventCreated || user?.uid ? window.origin.includes("://test.") ? process.env.NEXT_PUBLIC_EVENTSAPP?.replace("//", "//test") ?? "" : process.env.NEXT_PUBLIC_EVENTSAPP ?? "" : "/"
         },
         {
           icon: <SalirIcon />,
