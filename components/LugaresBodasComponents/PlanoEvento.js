0f72700f147f0cc61bdcebc45cd9a0f7670facc3
Jafet <febmerlib@gmail.com>
2023-12-08 20:32:48 +0100

    adding page invitaciones into cms

--- components/LugaresBodasComponents/PlanoEvento.js
+++ components/LugaresBodasComponents/PlanoEvento.js
@@ -6,7 +6,6 @@ import { InfoPlanoEventoPage } from "./PlanoEventoComponents/InfoPlanoEventoPage
 
 export const PlanoEvento = ({ componentState, setComponentState }) => {
     const path = window?.origin?.includes("://testcms.") ? process.env.NEXT_PUBLIC_EVENTSAPP?.replace("//", "//test") ?? "" : process.env.NEXT_PUBLIC_EVENTSAPP ?? ""
-    console.log(path)
     return (
         <div className="h-full">
             <iframe src={`${path}/mesas/?show=iframe`} width={"100%"} height={"100%"} ></iframe>
