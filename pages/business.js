d1ad16f5f2815f698dc89b9f792b22d91a12f490
eduardo andres <adch.edch@gmail.com>
2023-12-21 17:50:22 -0400

    working

--- pages/business.js
+++ pages/business.js
@@ -2,18 +2,20 @@ import { useState } from "react";
 import { SubmenuComponent } from "../components/CateringBodasComponents/SubmenuComponent"
 import { MarcasControl, MarcasTable } from "../components/ModuloMarcas/Marcas";
 import { CustomWebsControl } from "../components/ModuloMarcas/WeddingCustomWebs";
+import { PiBrowsers, PiCertificate } from "react-icons/pi";
+
 
 const Business = () => {
     const [optionSelect, setOptionSelect] = useState(0)
 
     const dataComponents = [
         {
-            icon: "",
+            icon: <PiCertificate className="h-6 w-auto" />,
             title: "Marcas",
             component: <MarcasControl />
         },
         {
-            icon: "",
+            icon: <PiBrowsers className="h-6 w-auto" />,
             title: "Wedding page",
             component: <CustomWebsControl/>
         },
@@ -23,7 +25,7 @@ const Business = () => {
     };
     return (
         <div className="md:grid md:grid-cols-6 h-[100vh]">
-            <SubmenuComponent dataComponents={dataComponents} onClick={handleClickOption} />
+            <SubmenuComponent dataComponents={dataComponents} onClick={handleClickOption} optionSelect={optionSelect} />
             <div className="col-span-6 md:col-span-5 z-10 px-5 py-2  h-[calc(100%-80px)] overflow-auto">
                 {dataComponents[optionSelect].component}
             </div>
