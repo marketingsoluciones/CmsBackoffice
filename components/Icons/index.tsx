22f41d29f5b64b72922e8740b54eed60fc04e552
eduardo andres <adch.edch@gmail.com>
2023-12-26 16:24:29 -0400

    update profile

--- components/Icons/index.tsx
+++ components/Icons/index.tsx
@@ -2102,6 +2102,16 @@ export const ArrowDownIcon: FC<PropsIcon> = (props) => {
   )
 }
 
+export const ArrowDownBodasIcon: FC<PropsIcon> = (props) => {
+  const { width = 1 } = props
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" {...props}>
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={width} d="M9 5l7 7-7 7" />
+    </svg>
+
+  )
+}
+
 export const ArrowLeft: FC<PropsIcon> = (props) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" {...props}>
