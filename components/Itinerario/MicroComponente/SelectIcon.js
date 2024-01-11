d4cddc4238de441c5280f641ea0e0cf4afd8cde4
eduardo andres <adch.edch@gmail.com>
2024-01-11 17:05:43 -0400

    working

--- components/Itinerario/MicroComponente/SelectIcon.js
+++ components/Itinerario/MicroComponente/SelectIcon.js
@@ -1,17 +1,127 @@
+import { Anillos, Baile, Baile2, Brindis, Carro, Cena, Cocteles, Comida, Dividersvg, Fotografo, FuegosArtificiales, Iglesia, Maquillaje, Merienda, Novios, PlusIcon, Salida, SesionFotos, Sol, Torta, Vestido, Dress, Dots } from "../../Icons/index"
+
 import { useField } from "formik";
 import { AddIcon } from "../../Icons/index"
+import { Modal } from "../../modals/Modal";
+import { IconList } from "./IconList";
+import { useEffect, useState } from "react";
 
-export const SelectIcon = ({ resultadoIcon, setOpenIcon, openIcon, ...props }) => {
+const IconArray = [
+    {
+        title: "Anillos",
+        icon: <Anillos />,
+    },
+    {
+        title: "FuegosArtificiales",
+        icon: <FuegosArtificiales />,
+    },
+    {
+        title: "Baile",
+        icon: <Baile />,
+    },
+    {
+        title: "Baile2",
+        icon: <Baile2 />,
+    },
+    {
+        title: "Brindis",
+        icon: <Brindis />,
+    },
+    {
+        title: "Carro",
+        icon: <Carro />,
+    },
+    {
+        title: "Cena",
+        icon: <Cena />
+    },
+    {
+        title: "Cocteles",
+        icon: <Cocteles />,
+    },
+    {
+        title: "Comida",
+        icon: <Comida />,
+    },
+    {
+        title: "Fotografo",
+        icon: <Fotografo />,
+    },
+    {
+        title: "Iglesia",
+        icon: <Iglesia />,
+    },
+    {
+        title: "Maquillaje",
+        icon: <Maquillaje />,
+    },
+    {
+        title: "Merienda",
+        icon: <Merienda />,
+    },
+    {
+        title: "Novios",
+        icon: <Novios />,
+    },
+    {
+        title: "Salida",
+        icon: <Salida />,
+    },
+    {
+        title: "SesionFotos",
+        icon: <SesionFotos />,
+    },
+    {
+        title: "Sol",
+        icon: <Sol />,
+    },
+    {
+        title: "Torta",
+        icon: <Torta />,
+    },
+    {
+        title: "Vestido",
+        icon: <Vestido />,
+    },
+    {
+        title: "Dress",
+        icon: <Dress />,
+    },
+]
+
+export const SelectIcon = ({ handleChange ,...props }) => {
     const [field, meta, helpers] = useField({ name: props?.name });
+    const [selectIcon, setSelectIcon] = useState(field?.value)
+    const [resultadoIcon, setResultadoIcon] = useState()
+    const [openIcon, setOpenIcon] = useState(false)
+
+    useEffect(() => {
+        setResultadoIcon(IconArray.find((elem) => elem?.title === selectIcon))
+        helpers.setValue(selectIcon)
+        handleChange("icon", selectIcon)
+    }, [selectIcon])
+
+    
+
     return (
         <div className="flex justify-center items-center">
             {resultadoIcon?.icon
-                ? <div className='w-10 md:w-12 lg:w-14 h-10 md:h-12 lg:h-14 cursor-pointer flex items-center justify-center ' onClick={() => setOpenIcon(!openIcon)}>
+                ? <div className='w-10 md:w-12 lg:w-14 h-10 md:h-12 lg:h-14 cursor-pointer flex items-center justify-center '
+                    onClick={() => {
+                        setOpenIcon(!openIcon)
+
+                    }} {...props}>
                     {resultadoIcon?.icon}
                 </div>
                 : <div className='w-10 md:w-12 lg:w-14 h-10 md:h-12 lg:h-14 cursor-pointer flex items-center justify-center text-gray-600 hover:text-gray-800' onClick={() => setOpenIcon(!openIcon)}>
                     <AddIcon />
                 </div>
+            }
+            {openIcon
+                ? <Modal openIcon={openIcon} setOpenIcon={setOpenIcon} classe={"h-max md:w-[30%]"} >
+                    <IconList IterArry={IconArray} openIcon={openIcon} setOpenIcon={setOpenIcon} setSelectIcon={setSelectIcon} />
+                </Modal>
+                : null
             }
         </div>
     )
--- components/Itinerario/MicroComponente/Task.js
+++ components/Itinerario/MicroComponente/Task.js
@@ -1,4 +1,3 @@
-import { Anillos, Baile, Baile2, Brindis, Carro, Cena, Cocteles, Comida, Dividersvg, Fotografo, FuegosArtificiales, Iglesia, Maquillaje, Merienda, Novios, PlusIcon, Salida, SesionFotos, Sol, Torta, Vestido, Dress, Dots } from "../../Icons/index"
 import { Form, Formik } from "formik";
 import { SubHeader } from "./SubHeader";
 import { AddEvent, Description, Duration, GuardarButtom, IconList, Responsable, ResponsableList, SelectIcon, Time, Tips } from ".";
@@ -9,88 +8,7 @@ import { EventContextProvider } from "../../../context/EventContext";
 import { fetchApiEventos, queries } from "../../../utils/Fetching";
 import { string } from "yup";
 
-const IconArray = [
-  {
-    title: "Anillos",
-    icon: <Anillos />,
-  },
-  {
-    title: "FuegosArtificiales",
-    icon: <FuegosArtificiales />,
-  },
-  {
-    title: "Baile",
-    icon: <Baile />,
-  },
-  {
-    title: "Baile2",
-    icon: <Baile2 />,
-  },
-  {
-    title: "Brindis",
-    icon: <Brindis />,
-  },
-  {
-    title: "Carro",
-    icon: <Carro />,
-  },
-  {
-    title: "Cena",
-    icon: <Cena />
-  },
-  {
-    title: "Cocteles",
-    icon: <Cocteles />,
-  },
-  {
-    title: "Comida",
-    icon: <Comida />,
-  },
-  {
-    title: "Fotografo",
-    icon: <Fotografo />,
-  },
-  {
-    title: "Iglesia",
-    icon: <Iglesia />,
-  },
-  {
-    title: "Maquillaje",
-    icon: <Maquillaje />,
-  },
-  {
-    title: "Merienda",
-    icon: <Merienda />,
-  },
-  {
-    title: "Novios",
-    icon: <Novios />,
-  },
-  {
-    title: "Salida",
-    icon: <Salida />,
-  },
-  {
-    title: "SesionFotos",
-    icon: <SesionFotos />,
-  },
-  {
-    title: "Sol",
-    icon: <Sol />,
-  },
-  {
-    title: "Torta",
-    icon: <Torta />,
-  },
-  {
-    title: "Vestido",
-    icon: <Vestido />,
-  },
-  {
-    title: "Dress",
-    icon: <Dress />,
-  },
-]
+
 
 const ResponsablesArry = [
   {
@@ -120,13 +38,9 @@ const ResponsablesArry = [
 ]
 
 export const Task = ({ itinerario, task, date }) => {
-  const [openIcon, setOpenIcon] = useState(false)
-  const resultadoIcon = IconArray.find(elem => elem.title === selectIcon);
-  const [selectIcon, setSelectIcon] = useState(null)
   const [openResponsableList, setOpenResponsableList] = useState(false)
   const { event, setEvent } = EventContextProvider()
-  console.log(resultadoIcon)
-  console.log(selectIcon)
+
   const initialValues = {
     icon: !task?.icon ? "" : task?.icon,
     time: !task?.hora ? "" : task?.hora,
@@ -161,14 +75,15 @@ export const Task = ({ itinerario, task, date }) => {
 
   return (
     <>
-      {/* <span> {JSON.stringify({ _id: task._id, hora: task.hora }, null, 2)}</span> */}
+
       <Formik enableReinitialize initialValues={initialValues} >
         {({ values, }) => {
           return (
             <Form>
+              {/* <AutoFormik values={values} /> */}
               <div className="grid grid-cols-1 lg:grid-cols-12 items-center justify-center md:px-20 lg:px-20 2xl:px-56 py-1" >
                 <div className="flex lg:col-span-7 justify-end">
-                  <SelectIcon openIcon={openIcon} setOpenIcon={setOpenIcon} resultadoIcon={resultadoIcon} />
+                  <SelectIcon name="icon" handleChange={handleBlurData} />
                   <div className="flex flex-col justify-center">
                     <InputTime name="time" onBlur={() => { handleBlurData("hora", values.time) }} />
                     <Duration name="duration" onBlur={() => { handleBlurData("duracion", values.duration.toString()) }} />
@@ -179,12 +94,7 @@ export const Task = ({ itinerario, task, date }) => {
                 <div className="flex lg:col-span-5">
                   <Tips name="tips" onBlur={() => { handleBlurData("tips", values.tips) }} />
                 </div>
-                {openIcon
-                  ? <Modal openIcon={openIcon} setOpenIcon={setOpenIcon} classe={"h-max md:w-[30%]"} >
-                    <IconList IterArry={IconArray} openIcon={openIcon} setOpenIcon={setOpenIcon} setSelectIcon={setSelectIcon} />
-                  </Modal>
-                  : null
-                }
+
                 {openResponsableList
                   ? <Modal openIcon={openResponsableList} setOpenIcon={setOpenResponsableList} classe={"h-max w-[16%]"} >
                     <ResponsableList DataArry={ResponsablesArry} openModal={openResponsableList} setOpenModal={setOpenResponsableList} />
@@ -199,3 +109,10 @@ export const Task = ({ itinerario, task, date }) => {
     </>
   )
 }
+
+/* const AutoFormik = ({ values }) => {
+  useEffect(() => {
+    console.log(values.icon)
+  }, [values])
+  return (null)
+} */
