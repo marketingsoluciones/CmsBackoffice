c64c2c206fd1ef578f87d4aff1bc68beb60dfad0
eduardo andres <adch.edch@gmail.com>
2024-01-17 13:27:37 -0400

    working on insidencia Bodapp-843

--- components/Calendario.js
+++ components/Calendario.js
@@ -6,19 +6,8 @@ export const Calendario = () => {
     const [state, setState] = useState(false)
     return (
         <>
-            
-            <div className="md:w-full px-5 py-2 space-y-2 ">
-                {(() => {
-                    if (!state) {
-                        return (
-                            <InfoPageCalendario />
-                        )
-                    } else {
-                        return(
-                          "hoal"
-                        )
-                    }
-                })()}
+            <div className="md:w-full h-[calc(100%-49px)] px-5 py-2 space-y-2 ">
+                {!state ? <InfoPageCalendario /> : "hola"}
             </div>
         </>
     )
