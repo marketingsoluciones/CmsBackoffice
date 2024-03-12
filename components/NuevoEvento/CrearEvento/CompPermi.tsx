65c4d3f6e01d0015530193c89ed51266540f96bb
ChristianWork05 <christian.lanza@recargaexpress.com>
2024-03-12 13:46:13 -0400

    avance 4

--- components/NuevoEvento/CrearEvento/CompPermi.tsx
+++ components/NuevoEvento/CrearEvento/CompPermi.tsx
@@ -3,9 +3,9 @@ import { memo, useMemo, type CSSProperties, FC } from "react";
 type CompPermiType = {
   icon?: string;
   administrador?: string;
-  l?: string;
-  christian?: string;
-  lanza?: string;
+  letra?: string;
+  nombre1?: string;
+  apellido1?: string;
 
   /** Style props */
   propBackgroundColor?: CSSProperties["backgroundColor"];
@@ -18,9 +18,9 @@ const CompPermi: FC <CompPermiType> = memo(
   ({
     icon,
     administrador,
-    l,
-    christian,
-    lanza,
+    letra,
+    nombre1,
+    apellido1,
     propBackgroundColor,
     propDisplay,
     propWidth,
@@ -46,9 +46,9 @@ const CompPermi: FC <CompPermiType> = memo(
     }, [propBackgroundColor1]);
 
     return (
-      <div className="self-stretch flex-1 rounded-2xs-5 bg-pro-fourvenues-com-beach-aguilas-create-event-1318x573-default-mystic-200 overflow-hidden flex flex-col items-start justify-start pt-0 px-0 pb-2.5 gap-[11px] text-left text-sm text-pro-fourvenues-com-beach-aguilas-create-event-1318x573-default-nero font-pro-fourvenues-com-beach-aguilas-dlsj9ud510272017c1tbi7y1lvzlroll-settings-rates-signatures-1318x573-default-1-inter-semi-bold-123">
+      <div className="self-stretch flex-1 rounded-md bg-slate-200 overflow-hidden flex flex-col items-start justify-start pt-0 px-0 pb-2.5 gap-[11px] text-left text-sm text-white font-semibold">
         <div
-          className="self-stretch bg-darkorange flex flex-row items-center justify-start py-[7px] px-[11px] gap-[1px]"
+          className="self-stretch flex flex-row items-center justify-start py-[7px] px-[11px] gap-[5px]"
           style={CompPermiStyle}
         >
           <img
@@ -64,19 +64,19 @@ const CompPermi: FC <CompPermiType> = memo(
           </div>
         </div>
         <div className="self-stretch flex-1 flex flex-col items-start justify-start py-0 px-2 text-base font-roboto">
-          <div className="flex-1 rounded-2xs-5 bg-pro-fourvenues-com-beach-aguilas-create-event-1318x573-default-nero shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)] flex flex-col items-start justify-start py-[7px] px-[10.5px]">
-            <div className="flex-1 flex flex-row items-center justify-start gap-[11px]">
+          <div className="rounded-md bg-white shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)] flex flex-col items-start justify-start py-[7px] px-[10.5px]">
+            <div className="flex flex-row items-center justify-start gap-[11px]">
               <div
-                className="self-stretch w-9 rounded-17xl bg-darkorange overflow-hidden shrink-0 flex flex-col items-center justify-center"
+                className="self-stretch w-9 rounded-full bg-darkorange overflow-hidden shrink-0 flex flex-col items-center justify-center"
                 style={CompPermiimageStyle}
               >
-                <b className="w-[9px] h-7 relative tracking-[0.15px] leading-[175%] inline-block">
-                  {l}
+                <b className="w-7 h-7 items-center justify-center  relative tracking-[0.15px] leading-[175%] inline-block">
+                  {letra}
                 </b>
               </div>
-              <div className="h-[21px] w-[106px] relative text-sm leading-[21px] text-pro-fourvenues-com-beach-aguilas-create-event-1318x573-default-black inline-block font-pro-fourvenues-com-beach-aguilas-dlsj9ud510272017c1tbi7y1lvzlroll-settings-rates-signatures-1318x573-default-1-inter-semi-bold-123">
-                <b>{christian}</b>
-                <span>{lanza}</span>
+              <div className="h-[21px] w-[106px] relative text-sm leading-[21px] text-black inline-block font-pro-fourvenues-com-beach-aguilas-dlsj9ud510272017c1tbi7y1lvzlroll-settings-rates-signatures-1318x573-default-1-inter-semi-bold-123">
+                <b className="font-semibold">{nombre1}</b>
+                <span className="font-normal">{apellido1}</span>
               </div>
             </div>
           </div>
