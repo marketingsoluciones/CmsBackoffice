f71f99bd5b3bc096d3c83dff4b471a8c183fb80a
eduardo andres <adch.edch@gmail.com>
2024-03-13 11:31:27 -0400

    css fixed on Marcas modul

--- components/MarcaBlancaMetricool/Iframe.js
+++ components/MarcaBlancaMetricool/Iframe.js
@@ -2,7 +2,7 @@ import { InfoPageMetricool } from "./InfoPageMetricool"
 
 export const IframeMetricool = ({ dataMetricool }) => {
     return (
-        <div className="h-full"  >
+        <div className="h-full w-full"  >
             {
                 dataMetricool != undefined ?
                     <iframe src={dataMetricool && dataMetricool?.whiteLabelLink} width={"100%"} className="h-[89vh] md:h-[100%]"></iframe> :
--- components/ModuloMarcas/WeddingCustomWebs/TablePagesList.js
+++ components/ModuloMarcas/WeddingCustomWebs/TablePagesList.js
@@ -14,38 +14,41 @@ export const TablePegesList = ({ setComponentState }) => {
     const [seteador, setSeteador] = useState(() => () => { })
 
     return (
-        <div className="px-5 py-2">
-            {state?.type === "view" && <div className="space-y-4 w-full h-full" >
-                <div>
-                    <div onClick={() => router.push("/")} className="w-5 h-5  text-gray-700 cursor-pointer">
-                        <ArrowLeft />
+        <>
+            {
+                state?.type === "view" &&
+                    <div className="space-y-4 w-full h-full" >
+                        <div>
+                            <div onClick={() => router.push("/")} className="w-5 h-5  text-gray-700 cursor-pointer">
+                                <ArrowLeft />
+                            </div>
+                            <p className=" mt-1 text-3xl text-rosa">
+                                Tus Paginas Web
+                            </p>
+                            <p className=" text-sm bg-white p-2 rounded-lg">Administra tus Webs Pages.  </p>
+                        </div>
+                        <div className=" md:relative ">
+                            <button onClick={() => router.push("/marketplace")} className="bg-rosa rounded-lg px-4 py-1 text-white text-base " >
+                                Añadir Pagina
+                            </button>
+                            <div className=" absolute h-8  rounded-md px-2 flex items-center  border-gray-400 border-2  bottom-0 right-0 w-1/3 ">
+                                <SearchIcon />
+                                <GlobalFilter
+                                    globalFilter={global}
+                                    setGlobalFilter={seteador}
+                                />
+                            </div>
+                        </div>
+                        <div className="bg-white space-y-5 rounded-xl flex flex-col py-2 px-4  ">
+                            <OnlyViewTable slug={"customWebBuilder"} dispatch={dispatch} setbuscador={setSeteador} />
+                        </div>
                     </div>
-                    <p className=" mt-1 text-3xl text-rosa">
-                        Tus Paginas Web
-                    </p>
-                    <p className=" text-sm bg-white p-2 rounded-lg">Administra tus Webs Pages.  </p>
-                </div>
-                <div className=" md:relative">
-                    <button onClick={() => router.push("/marketplace")} className="bg-rosa rounded-lg px-4 py-1 text-white text-base " >
-                        Añadir Pagina
-                    </button>
-                    <div className=" absolute h-8  rounded-md px-2 flex items-center  border-gray-400 border-2  bottom-0 right-0 w-1/3 ">
-                        <SearchIcon />
-                        <GlobalFilter
-                            globalFilter={global}
-                            setGlobalFilter={seteador}
-                        />
-                    </div>
-                </div>
-                <div className="bg-white space-y-5 rounded-xl flex flex-col py-2 px-4  ">
-                    <OnlyViewTable slug={"customWebBuilder"} dispatch={dispatch} setbuscador={setSeteador} />
-                </div>
-            </div>
             }
-            {state?.type === "edit" && (
-                <WebBuilder id={state.data._id} />
-            )
+            {
+                state?.type === "edit" && (
+                    <WebBuilder id={state.data._id} />
+                )
             }
-        </div>
+        </>
     )
 }
