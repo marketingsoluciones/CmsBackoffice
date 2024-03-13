4aba8ff93603de0f16dc2a457f539a85b245ddc2
eduardo andres <adch.edch@gmail.com>
2024-03-13 13:18:03 -0400

    refactoring on cateringBodas

---
+++ components/CateringBodasComponents/index.js
@@ -0,0 +1,8 @@
+export * from "./CartaProcutos"
+export * from "./InfoCateringBodas"
+export * from "./InvitadosC"
+export * from "./ItinerarioC"
+export * from "./MenuEmpresa"
+export * from "./PlantillaMenu"
+export * from "./SubmenuComponent"
+
--- pages/cateringBodas.js
+++ pages/cateringBodas.js
@@ -1,9 +1,74 @@
-import { CateringBodas } from "../components/CateringBodas"
+import { useEffect, useState } from "react"
+import { EventsGroupContextProvider } from "../context/EventsGroupContext"
+import { useRouter } from "next/router"
+import { InvitadosCatering, ItinerarioCatering, MenuCatering, PlatillOpenCatering, PlatilloCloseCatering } from "../components/Icons/index"
+import { CartaProducto, InfoCateringBodas, ItinerarioC, MenuEmpresa, PlantillaMenu, SubmenuComponent } from "../components/CateringBodasComponents"
+import { IframeApp } from "../layouts/IframeApp"
+import { Modal } from "../components/modals/Modal"
+import { ContactarGold } from "../components/formularios/ContactarGold"
 
 const cateringBodasPage = () => {
-    return(
+    const [optionSelect, setOptionSelect] = useState(5)
+    const [modalContacto, setModalContacto] = useState(false)
+    const { eventsGroup } = EventsGroupContextProvider()
+    const router = useRouter()
+    const { stateOriginPath } = router?.query
+    useEffect(() => {
+        if (stateOriginPath) {
+            setOptionSelect(stateOriginPath)
+        }
+    }, [])
+    const dataComponents = [
+        {
+            icon: <PlatillOpenCatering />,
+            title: "Carta de productos",
+            component: <CartaProducto setComponentState={setOptionSelect} />
+        },
+        {
+            icon: <MenuCatering />,
+            title: "Plantillas de menú",
+            component: <PlantillaMenu setComponentState={setOptionSelect} />
+        },
+        {
+            icon: <PlatilloCloseCatering />,
+            title: "Menú",
+            component: <MenuEmpresa setComponentState={setOptionSelect} />
+        },
+        {
+            icon: <InvitadosCatering />,
+            title: "Lista de Invitados",
+            component: <IframeApp route="invitados" />,//<ListaInvitados setComponentState={setOptionSelect} eventsGroup={eventsGroup} />
+            type: "iframe",
+        },
+        {
+            icon: <ItinerarioCatering />,
+            title: "Itinerarios",
+            component: <ItinerarioC setComponentState={setOptionSelect} />
+        },
+        {
+            component: <InfoCateringBodas setOptionSelect={setOptionSelect} modalContacto={modalContacto} setModalContacto={setModalContacto} />
+        }
+    ]
+    const handleClickOption = (idx) => {
+        setOptionSelect(idx);
+    };
+    const newArryDataComponents = dataComponents.slice()
+    newArryDataComponents.splice(5, 1)
+    return (
         <>
-            <CateringBodas/>
+            <div className={`flex h-full ${dataComponents[optionSelect].type !== "iframe" && "w-full"}`}>
+                {optionSelect > -1 &&
+                    <SubmenuComponent dataComponents={newArryDataComponents} optionSelect={optionSelect} onClick={handleClickOption} />
+                }
+                <div className="flex-1">
+                    {dataComponents[optionSelect]?.component}
+                </div>
+            </div >
+            {modalContacto &&
+                <Modal classe={"w-[28%] h-[86%]"}>
+                    <ContactarGold openModal={modalContacto} setOpenModal={setModalContacto} />
+                </Modal>
+            }
         </>
     )
 }
