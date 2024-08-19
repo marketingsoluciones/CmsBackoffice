230d6c86bc20f520b03863c1eafb99aed63cab91
eduardo andres <adch.edch@gmail.com>
2024-08-19 15:05:28 -0400

    working on add iframe facturacion

--- pages/facturacion.js
+++ pages/facturacion.js
@@ -1,9 +1,15 @@
 import { ModulosFacturacion } from "../components/FacturacionComponents/ModulosFacturacion"
+import { SocketContextProvider } from "../context"
 
 const Facturacion = () => {
+    const { fatherID } = SocketContextProvider()
+
+    const path = window?.origin?.includes("://testcms.") ? process.env.NEXT_PUBLIC_EVENTSAPP?.replace("//", "//test") ?? "" : process.env.NEXT_PUBLIC_EVENTSAPP ?? ""
+
     return (
         <div className="px-5 py-2 ">
-            <ModulosFacturacion />
+            {/* <ModulosFacturacion /> */}
+            <iframe src={`${path}/facturacion/?show=iframe&father=${fatherID}`} width={"100%"} className="h-[89vh] md:h-[100%]"></iframe>
         </div>
     )
 }
