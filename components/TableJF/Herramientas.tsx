198bce8271a059fe8c105c89e902cdd387366742
eduardo andres <adch.edch@gmail.com>
2024-08-26 12:02:08 -0400

    working

--- components/TableJF/Herramientas.tsx
+++ components/TableJF/Herramientas.tsx
@@ -29,11 +29,16 @@ export const Filter = ({ setColumnsView, columnsView, table }) => {
     return (
         <div className="relative">
             <HiOutlineFilter onClick={() => setColumnsView(!columnsView)} className="w-6 h-6 cursor-pointer" />
-            <div className="bg-gray-200 shadow-lg rounded-xl absolute -translate-x-[132px] translate-y-2" >
-                {columnsView &&
-                    <ClickAwayListener onClickAway={() => columnsView && setColumnsView(!setColumnsView)}>
-                        <div className="bg-white w-36 h-max m-2 inline-block border border-black shadow rounded space-y-1">
-                            <div className="px-1 py-2 border-b border-black flex space-x-2">
+            {columnsView &&
+                <ClickAwayListener onClickAway={() => columnsView && setColumnsView(!setColumnsView)}>
+                    <div className="bg-white w-[250px] h-max border border-gray-300 shadow rounded-md absolute -translate-x-[200px] translate-y-2 " >
+                        <div className="px-3 pt-3">
+                            <label className="font-semibold text-[14px]">
+                                Customise Columns
+                            </label>
+                        </div>
+                        <div className="grid grid-cols-2 p-3 ">
+                            <div className="flex space-x-1 items-center pl-1">
                                 <input
                                     {...{
                                         type: 'checkbox',
@@ -45,11 +50,69 @@ export const Filter = ({ setColumnsView, columnsView, table }) => {
                                     Toggle All
                                 </label>
                             </div>
-                            <div className="py-1 border-b border-black">
-                                {table.getAllLeafColumns().map(column => {
-                                    return (
-                                        <div key={column.id} className="px-1 py-1 flex space-x-1">
+                        </div>
+                        <div className="px-3 py-2 bg-gray-100">
+                            <label className="font-semibold">
+                                Columnas Disponibles
+                            </label>
+                        </div>
+                        <div className="p-3 ">
+                            {table.getAllLeafColumns().map(column => {
+                                return (
+                                    <div key={column.id} className="px-1 py-1 flex space-x-1">
 
+                                        <input
+                                            {...{
+                                                type: 'checkbox',
+                                                checked: column.getIsVisible(),
+                                                onChange: column.getToggleVisibilityHandler(),
+                                            }}
+                                        />{' '}
+                                        <label>
+                                            {column.id}
+                                        </label>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className="flex items-center justify-between p-3 bg-gray-100">
+                            <div className="flex items-center space-x-2 font-semibold">
+                                <TbFilterPlus className="h-5 w-5" />
+                                <label >
+                                    Crear filtro
+                                </label>
+                            </div>
+                            <div>
+                                <button className="bg-gray-200 px-2 py-2 rounded-lg hover:bg-gray-300">
+                                    Guardar
+                                </button>
+                            </div>
+                        </div>
+
+                    </div>
+                </ClickAwayListener>
+            }
+        </div >
+    )
+}
+
+export const FavoriteFilter = ({ setFavoriteFilterView, favoriteFilterView, table }) => {
+    return (
+        <div className="relative ">
+            <TbFilterHeart onClick={() => setFavoriteFilterView(!favoriteFilterView)} className="w-6 h-6 cursor-pointer" />
+            {favoriteFilterView &&
+                <ClickAwayListener onClickAway={() => favoriteFilterView && setFavoriteFilterView(!favoriteFilterView)}>
+                    <div className=" bg-white w-[250px] h-max border border-gray-300 shadow rounded-md absolute -translate-x-[200px] translate-y-2 " >
+                        <div className="px-3 pt-3">
+                            <label className="font-semibold text-[14px]">
+                                Vistas 
+                            </label>
+                        </div>
+                        <div className="p-3 ">
+                            {table.getAllLeafColumns().map(column => {
+                                return (
+                                    <div className="flex justify-between pr-2">
+                                        <div key={column.id} className="px-1 py-1 flex items-center justify-center space-x-1">
                                             <input
                                                 {...{
                                                     type: 'checkbox',
@@ -61,68 +124,30 @@ export const Filter = ({ setColumnsView, columnsView, table }) => {
                                                 {column.id}
                                             </label>
                                         </div>
-                                    )
-                                })}
-                            </div>
-                            <div className="py-2">
-                                <div className="flex mx-1 p-0.5 rounded-lg items-center justify-center cursor-pointer space-x-1.5 bg-slate-300  hover:bg-slate-200 ">
-                                    <TbFilterPlus />
-                                    <label className="cursor-pointer">
-                                        Añadir a favorito
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                    </ClickAwayListener>
-                }
-            </div>
-        </div >
-    )
-}
-
-export const FavoriteFilter = ({ setFavoriteFilterView, favoriteFilterView, table }) => {
-    return (
-        <div className="relative ">
-            <TbFilterHeart onClick={() => setFavoriteFilterView(!favoriteFilterView)} className="w-6 h-6 cursor-pointer" />
-            <div className="bg-gray-200 shadow-lg rounded-xl absolute -translate-x-[132px] translate-y-2" >
-                {favoriteFilterView &&
-                    <ClickAwayListener onClickAway={() => favoriteFilterView && setFavoriteFilterView(!favoriteFilterView)}>
-                        <div className="bg-white w-max h-max m-2 inline-block border border-black shadow rounded space-y-1">
-                            <div className="py-1 border-b border-black w-[170px]">
-                                {table.getAllLeafColumns().map(column => {
-                                    return (
-                                        <div className="flex justify-between pr-2">
-                                            <div key={column.id} className="px-1 py-1 flex items-center justify-center space-x-1">
-                                                <input
-                                                    {...{
-                                                        type: 'checkbox',
-                                                        checked: column.getIsVisible(),
-                                                        onChange: column.getToggleVisibilityHandler(),
-                                                    }}
-                                                />{' '}
-                                                <label>
-                                                    {column.id}
-                                                </label>
-                                            </div>
-                                            <div className="cursor-pointer">
-                                                <MdOutlineEdit className="h-6 w-4"/>
-                                            </div>
+                                        <div className="cursor-pointer">
+                                            <MdOutlineEdit className="h-6 w-4" />
                                         </div>
-                                    )
-                                })}
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className="flex items-center justify-between p-3 bg-gray-100">
+                            <div className="flex items-center space-x-2 font-semibold">
+                                <TbFilterX  className="h-5 w-5" />
+                                <label >
+                                    Eliminar Vistas
+                                </label>
                             </div>
-                            <div className="py-2">
-                                <div className="flex mx-1 p-0.5 rounded-lg items-center justify-center cursor-pointer space-x-1.5 bg-slate-300  hover:bg-slate-200 ">
-                                    <TbFilterX />
-                                    <label className="cursor-pointer">
-                                        Eliminar favorito
-                                    </label>
-                                </div>
+                            <div>
+                                <button className="bg-gray-200 px-2 py-2 rounded-lg hover:bg-gray-300">
+                                    Borrar
+                                </button>
                             </div>
                         </div>
-                    </ClickAwayListener>
-                }
-            </div>
+                    </div>
+                </ClickAwayListener>
+            }
+            
         </div >
     )
 }
