279183cd9b0969f8e30fccc7b3fc28cd6637bdb1
ChristianWork05 <christian.lanza@recargaexpress.com>
2024-08-28 10:17:03 -0400

    Eventos Errores-Inicio V1

--- components/NuevoEvento/EventosCreados/EventosCreados.tsx
+++ components/NuevoEvento/EventosCreados/EventosCreados.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import Component1 from "./Component1";
 import ModoDemo from "../Principal/ModoDemo";
 import TabGeneral from "./TabsGeneral";
+
 interface propsEventosCreados {
     componentState: any;
     setComponentState: any;
@@ -15,9 +16,10 @@ const EventosCreados: FC <propsEventosCreados> = ({componentState,setComponentSt
     <div className="w-[100%] bg-whitesmoke flex flex-col items-start justify-start py-[30px] px-[20px] box-border gap-[30px]">
       <ModoDemo/>
       <Component1 componentState={componentState} setComponentState={setComponentState} />
+
       
       <TabGeneral componentState={componentState} setComponentState={setComponentState}/>
-
+    
     </div>
   );
 };
